b0b9e2790617a281ef11305d2fb91c8f3bbf4472
median-dxz <med_iridescence@163.com>
2023-07-29 21:20:01 +0800

    fix: 修复css样式注入

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,110 +1,123 @@
 "use client";
-import {useRouter} from "next/router"
+import { config } from "@/config/config";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 import {
-    AppBar as MuiAppBar,
     Avatar,
     Box,
-    IconButton, Menu, MenuItem,
+    IconButton,
+    Menu,
+    MenuItem,
+    AppBar as MuiAppBar,
     Toolbar,
     Tooltip,
     Typography,
-    useColorScheme
+    useColorScheme,
 } from "@mui/material";
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import React, {useEffect, useMemo, useState} from "react";
-import {config} from "@/config/config";
 import Link from "next/link";
+import React, { useEffect, useMemo, useState } from "react";
 
 interface AppBarProps {
-    title: String,
-    login: boolean
+    title: String;
+    login: boolean;
 }
 
 const menu_items = [
-
     {
         name: config.appbar.profile_menu.Login,
         link: "/user/login",
-        login: false
+        login: false,
     },
     {
         name: config.appbar.profile_menu.Register,
         link: "/user/register",
         login: false,
     },
-]
+];
 
 const genMenuItems = (login: boolean) => {
-    return menu_items.filter(v => v.login == login).map(
-        (value, index) =>
+    return menu_items
+        .filter((v) => v.login == login)
+        .map((value, index) => (
             <Link href={value.link} key={index}>
                 <MenuItem>
-                        <Typography sx={{minWidth: "4rem", textAlign: "center"}}>{value.name}</Typography>
+                    <Typography sx={{ minWidth: "4rem", textAlign: "center" }}>
+                        {value.name}
+                    </Typography>
                 </MenuItem>
             </Link>
-    );
-}
+        ));
+};
 
-function AppBar({title, login}: AppBarProps) {
-    const [dark, setDark] = useState(true);
+function AppBar({ title, login }: AppBarProps) {
     const [menuAnchor, setMenuAnchor] = useState<HTMLElement | null>(null);
-    const {setMode} = useColorScheme();
+    const [mounted, setMounted] = useState(false);
+    const { setMode, mode } = useColorScheme();
 
-    useEffect(() => {
-        if (!dark)
-            setMode("light");
-        else
-            setMode("dark");
-
-    }, [dark, setMode]);
-
-    const menuItems = useMemo(
-        () => genMenuItems(login),
-        [login]
-    );
-
+    const menuItems = useMemo(() => genMenuItems(login), [login]);
 
     const ProfileMenu = useMemo(
-        () => <Menu
-            open={menuAnchor !== null}
-            anchorEl={menuAnchor}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            sx={{ minWidth: '15rem' }}
-            onClose={() => setMenuAnchor(null)}
-        >
-            {menuItems}
-        </Menu>
-        , [menuAnchor]);
+        () => (
+            <Menu
+                open={menuAnchor !== null}
+                anchorEl={menuAnchor}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                }}
+                sx={{ minWidth: "15rem" }}
+                onClose={() => setMenuAnchor(null)}
+            >
+                {menuItems}
+            </Menu>
+        ),
+        [menuAnchor]
+    );
 
-    return <>
-        <Box sx={{flexGrow: 1}}>
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
             <MuiAppBar position={"static"}>
                 <Toolbar>
-                    <Typography variant={"h5"} sx={{flexGrow: 1}}>
+                    <Typography variant={"h5"} sx={{ flexGrow: 1 }}>
                         {title}
                     </Typography>
 
-                    <Box sx={{flexGrow: 0}}>
+                    <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title={config.appbar.ProfileTooltip}>
                             <IconButton
-                                onClick={(event) => setMenuAnchor(event.target as HTMLElement)}
-                                sx={{p: 0}}>
-                                <Avatar/>
+                                onClick={(event) =>
+                                    setMenuAnchor(event.target as HTMLElement)
+                                }
+                                sx={{ p: 0 }}
+                            >
+                                <Avatar />
                             </IconButton>
                         </Tooltip>
                         {ProfileMenu}
                     </Box>
-                    <Box sx={{width: 2}}/>
-                    <IconButton onClick={() => setDark(!dark)}>
-                        {dark ? <LightModeIcon/> : <DarkModeIcon/>}
+                    <Box sx={{ width: 2 }} />
+                    <IconButton
+                        onClick={() => {
+                            if (mode === "light") {
+                                setMode("dark");
+                            } else {
+                                setMode("light");
+                            }
+                        }}
+                    >
+                        {mode === "dark" || !mounted ? (
+                            <LightModeIcon />
+                        ) : (
+                            <DarkModeIcon />
+                        )}
                     </IconButton>
                 </Toolbar>
             </MuiAppBar>
         </Box>
-    </>;
-};
-export {AppBar};
+    );
+}
+export { AppBar };
